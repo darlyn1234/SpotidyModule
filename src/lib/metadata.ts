5ddd7bd3851bde19ee561a154bdf904a5cbcbe78
Darling <89032212+darlyn1234@users.noreply.github.com>
2024-02-03 21:49:33 -0500

    Update metadata.ts

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,41 +1,34 @@
-import Ffmpeg from 'fluent-ffmpeg';
-import { renameSync, unlinkSync } from 'fs';
-import { IMetadata, ITrack } from '../typings';
+import Ffmpeg from 'fluent-ffmpeg'
+import { renameSync, unlinkSync } from 'fs'
+import { IMetadata, ITrack } from '../typings'
 
 export default async (data: ITrack, file: string): Promise<string> => {
-    const outputOptions: string[] = ['-map', '0:0', '-codec', 'copy'];
+    const outputOptions: string[] = ['-map', '0:0', '-codec', 'copy']
 
     const metadata: IMetadata = {
         title: data.name,
         album: data.album_name,
         artist: data.artists,
         date: data.release_date
-    };
-
-    Object.keys(metadata).forEach((key) => {
-        outputOptions.push('-metadata', `${String(key)}=${metadata[key as 'title' | 'artist' | 'date' | 'album']}`);
-    });
-
-    if (data.cover_image) {
-        outputOptions.push('-attach', data.cover_image, '-metadata:s', 'cover=');
+        //attachments: []
     }
 
-    const out = `${file.split('.')[0]}_temp.mp3`;
-    
+    Object.keys(metadata).forEach((key) => {
+        outputOptions.push('-metadata', `${String(key)}=${metadata[key as 'title' | 'artist' | 'date' | 'album']}`)
+    })
+
+    const out = `${file.split('.')[0]}_temp.mp3`
     await new Promise((resolve, reject) => {
         Ffmpeg()
             .input(file)
             .on('error', (err) => {
-                reject(err);
+                reject(err)
             })
             .on('end', () => resolve(file))
             .addOutputOptions(...outputOptions)
-            .saveToFile(out);
-    });
-
-    unlinkSync(file);
-    renameSync(out, file);
-
-    return file;
-};
-
+            .saveToFile(out)
+    })
+    unlinkSync(file)
+    renameSync(out, file)
+    return file
+}
